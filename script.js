c0a1bf2330bc118e7c757b712b8557e99df79359
sierra <sierra@atomicrobot.com>
2021-01-09 18:24:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 var imageURLs = [
   {
-    url: "https://github.com/droidswithdrinks/droidswithdrinks.github.io/blob/main/images/droids_coffee.png", 
+    url: "images/droids_coffee.png", 
     alt: "Two androids cheersing coffee mugs", 
     title: "Coffee and coding"
   }, 
   {
-    url: "https://github.com/droidswithdrinks/droidswithdrinks.github.io/blob/main/images/droids_icecream.png", 
+    url: "images/droids_icecream.png", 
     alt: "Two androids cheersing ice cream cones", 
     title: "Ice Cream Sandwich"
   },
   {
-    url: "https://github.com/droidswithdrinks/droidswithdrinks.github.io/blob/main/images/droids_margs.png", 
+    url: "images/droids_margs.png", 
     alt: "Two androids cheersing margaritas", 
     title: "Margaritas and mobile"
   }, 
   {
-    url: "https://github.com/droidswithdrinks/droidswithdrinks.github.io/blob/main/images/droids_martinis.png", 
+    url: "images/droids_martinis.png", 
     alt: "Two androids cheersing martinis", 
     title: "MAD martinis"
   }
