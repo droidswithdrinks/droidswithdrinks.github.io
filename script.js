cef8fea4384663281cd36bbde19e505670d1f1bc
Sierra Obryan <sierra@atomicrobot.com>
2021-01-09 19:14:56 -0500

    new images

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 var imageURLs = [
   {
-    url: "images/droids_coffee.png", 
-    alt: "Two androids cheersing coffee mugs", 
+    url: "images/droids_coffee.png",
+    alt: "Two androids cheersing coffee mugs",
     title: "Coffee and coding"
-  }, 
+  },
   {
-    url: "images/droids_icecream.png", 
-    alt: "Two androids cheersing ice cream cones", 
+    url: "images/droids_icecream.png",
+    alt: "Two androids cheersing ice cream cones",
     title: "Ice Cream Sandwich"
   },
   {
-    url: "images/droids_margs.png", 
-    alt: "Two androids cheersing margaritas", 
+    url: "images/droids_margs.png",
+    alt: "Two androids cheersing margaritas",
     title: "Margaritas and mobile"
-  }, 
+  },
   {
-    url: "images/droids_martinis.png", 
-    alt: "Two androids cheersing martinis", 
+    url: "images/droids_martinis.png",
+    alt: "Two androids cheersing martinis",
     title: "MAD martinis"
+  },
+  {
+    url: "images/droids_bloody.png",
+    alt: "Two androids cheersing bloody marys",
+    title: "Brunch Bunch"
+  },
+  {
+    url: "images/droids_bubble_tea.png",
+    alt: "Two androids cheersing bubble teas",
+    title: "procrasTEAnating"
+  },
+  {
+    url: "images/droid_miami.png",
+    alt: "Two androids cheersing miami vice cocktails",
+    title: "Five star cocktail"
+  },
+  {
+    url: "images/droids_smoothies.png",
+    alt: "Two androids cheersing smoothies",
+    title: "Smooth(l)ie running apps"
   }
 ];
 
